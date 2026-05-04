d3e3a481d65004f2947cb079a48d915847313cce
Mostafa Nouri <mostafa.nouri@elev.ga.ntig.se>
2026-05-04 10:43:44 +0200

    jj

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,30 @@
 package com.example;
-import java.net.http.HttpClient;
+//GSON objekt som vi behöver
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+// Importera Type för att hjälpa json att omvandla data
+import java.lang.reflect.Type;
+//UniREST objekt som vi behöver
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+import kong.unirest.UnirestException;
+// Importera Fil hantering
+import java.nio.file.*;
+import java.io.IOException;
+// ArrayList för att lagra objekt
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static String baseUrl = "http://10.151.168.5:3118";
+    private static Gson gson = new Gson(); // att översätta data
     private static ArrayList<Book> books = new ArrayList<>();
     private static ArrayList<Magazine> magazines = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         boolean running =true;
 
-        while (running = true){ 
+        while (running == true){ 
             System.out.println("\n--- Libarysystem Meny ---");
             System.out.println("1. Hämta böcker");
             System.out.println("2. Hämta tidningar");
@@ -19,7 +33,7 @@ public class Main {
             System.out.println("5. Lägg till bok");
             System.out.println("6. Lägg till tidning");
             System.out.println("7. Avsluta");
-            System.out.println("Skriv 1-6 för alternativ:")
+            System.out.println("Skriv 1-6 för alternativ:");
 
             String choice = scanner.nextLine();
 
@@ -53,7 +67,48 @@ public class Main {
             }
         }
     }
-private static void fetchData(String type) {
+private static void fetchBooks() {
+        HttpResponse<String> response = Unirest.get(baseUrl + "/books").asString();
         
+        Type bookListType = new TypeToken<ArrayList<Book>>(){}.getType();
+        books = gson.fromJson(response.getBody(), bookListType);
+        
+        System.out.println("Böcker har laddats in!");
     }
+
+private static void fetchmagazines() {
+        HttpResponse<String> response = Unirest.get(baseUrl + "/magazines").asString();
+        
+        Type magListType = new TypeToken<ArrayList<Magazine>>(){}.getType();
+        magazines = gson.fromJson(response.getBody(), magListType);
+        
+        System.out.println("Tidningar har laddats in!");
+    }
+
+private static void displayall(){
+    System.out.println("\nBöcker");
+    for(Book b : books)System.out.println(b.getTitle());
+    
+    System.out.println("\nTidingiar");
+for (Magazine m : magazines) System.out.println(m.getTitle());
 }
+
+private static void addNewBook() {
+        System.out.print("Titel: "); String t = scanner.nextLine();
+        System.out.print("Författare: "); String a = scanner.nextLine();
+        System.out.print("genre: "); String g = scanner.nextLine();
+        System.out.print("sidor: "); int p = Integer.parseInt(scanner.nextLine());
+
+        books.add(new Book(books.size() + 1, t, true,a ,g ,p));
+}
+
+private static void addNewMagazine() {
+        System.out.print("Titel: "); String t = scanner.nextLine();
+        System.out.print("Nummer: "); int n = Integer.parseInt(scanner.nextLine());
+        System.out.print("Kategori: "); String c = scanner.nextLine();
+        System.out.print("År: "); int y = Integer.parseInt(scanner.nextLine());
+
+    
+        magazines.add(new Magazine(books.size() + 1, t, true,n, c ,y));
+
+    }
